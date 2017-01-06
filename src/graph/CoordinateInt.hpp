ed0bc46118e328e7734fbd6c999142577e212896
ThomasvLingen <thovali96@gmail.com>
2017-01-06 14:13:11 +0100

    feat: add printing for CoordinateInt

--- src/graph/CoordinateInt.hpp
+++ src/graph/CoordinateInt.hpp
@@ -5,9 +5,13 @@
 #ifndef MINOR_KMINT_COORDINATEINT_HPP
 #define MINOR_KMINT_COORDINATEINT_HPP
 
+#include <ostream>
+
 struct CoordinateInt {
     int x;
     int y;
 };
+
+std::ostream& operator<<(std::ostream& os, CoordinateInt& cord);
 
 #endif //MINOR_KMINT_COORDINATEINT_HPP
